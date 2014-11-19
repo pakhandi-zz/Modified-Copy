cf968356994e9c3ec7450d41d22509a472e485be
Asim Krishna Prasad <pakhandi@users.noreply.github.com>
2014-11-19 23:13:00 +0530

    Bug Fix

--- major.sh
+++ major.sh
@@ -1,9 +1,9 @@
 echo "Enter the name of the source file : "
-read source
+read ssource
 echo "Enter the name of the destination file : "
 read destination
 echo "Enter the string to be replaced : "
 read target
 echo "Enter the string to take the place of the target string : "
-echo patch
-sed 's/'target'/'patch'/g' source > destination
+read spatch
+sed 's/'$target'/'$patch'/g' $source > $destination
